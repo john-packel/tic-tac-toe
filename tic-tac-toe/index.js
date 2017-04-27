ee80edf934f79bffa4399a0022d16e156c8f5740
John Packel <john.packel@gmail.com>
2017-04-27 16:27:43 -0400

    Last add before resuming work.

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -31,7 +31,6 @@ window.onload = function() {
       // check rows
       for(var r = 0; r < 7; r+=3) {
         console.log('r = ', r);
-
       if(checkArr.slice(r, r + 3).reduce(function(acc, val) {
         return acc + val;
       }) === 3) {
@@ -104,47 +103,9 @@ window.onload = function() {
     }
     player1 = !player1;
     plays++;
+    if(plays === 9) {
+      console.log('It\'s a draw! Click to play again.');
+    }
     console.log('plays = ', plays);
   }
-}
-
-
-
-
-
-
-
-var checkrows = function(playerboard) {
-  var row1 = playerboard.slice(0,3);
-  var row2
-  var row3
-
-  if(row1.reduce(function(acc, val) {
-    return acc + val;
-  }) === 3 || row2.reduce(function(acc, val) {
-    return acc + val;
-  }) === 3 || row3.reduce(function(acc, val) {
-    return acc + val;
-  }) === 3) {
-    alert("Player 1 wins!");
-  }
-}
-
-var checkForWin = function(player) {
-  if(player === 1) {
-    checkrows(player1);
-    checkcolumns(player1);
-    checkdiag(player1);
-  }
-}
-
-// with legal move, change value of correct index to 1; change other player same index to 'x'
-var player1Turn = function() {
-    var play = // get button # from on-click input
-    // if(player1[play] === 'x' || player1[play] === 1) {
-    //   alert("Illegal move");
-    // }
-    // else {player1[play] = 1}
-  // }
-  checkForWin(1);
 }
